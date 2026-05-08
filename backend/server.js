ff1ea5beffc28d4c8d1bcdef56cb8d3d4d2d63d7
nguyencodervn02 <41292507+NguyenCoderVN@users.noreply.github.com>
2026-05-09 05:00:15 +0700

    feat(be): initialize basic express server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("Server is running");
+import express from "express";
+
+const app = express();
+
+app.get("/", (_req, res) => {
+  res.send("Server is working");
+});
+
+app.listen(
+  5000,
+  console.log("Server is running on port 5000 number"),
+);
