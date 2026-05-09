5079d0a8cb0dbef4bdfa3c3a6eb4c502eb38648b
nguyencodervn02 <41292507+NguyenCoderVN@users.noreply.github.com>
2026-05-09 08:24:14 +0700

    feat(be): connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+
 import { authRoutes } from "./routes/auth.routes.js";
+import { connectMongoDB } from "./db/connectMongoDB.js";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes);
 
-app.listen(5000, () =>
-  console.log("Server is running on port 5000 number"),
-);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectMongoDB();
+});
