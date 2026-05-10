ca8a3cbbe7a7f0429b0b973272bcc9e2d6d6fd37
nguyencodervn02 <41292507+NguyenCoderVN@users.noreply.github.com>
2026-05-10 08:38:27 +0700

    feat(be): add followUnfollowUser

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -13,3 +13,39 @@ export const getUserProfile = async (req, res) => {
     return handleError(res, error, "getUserProfile");
   }
 };
+
+export const followUnfollowUser = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const user = await User.findById(userId);
+
+    const { followerId } = req.params;
+    const followerUser = await User.findById(followerId);
+
+    if (userId.toString() === followerId)
+      return res
+        .status(400)
+        .json({ error: "You cannot follow yourself" });
+
+    if (!user || !followerUser)
+      return res.status(404).json({ error: "User not found" });
+
+    const isFollowing = user.following.includes(followerId);
+
+    if (!isFollowing) {
+      await user.updateOne({ $push: { following: followerId } });
+      await followerUser.updateOne({ $push: { followers: userId } });
+      return res
+        .status(200)
+        .json({ message: "User followed successfully" });
+    } else {
+      await user.updateOne({ $pull: { following: followerId } });
+      await followerUser.updateOne({ $pull: { followers: userId } });
+      return res
+        .status(200)
+        .json({ message: "User unfollowed successfully" });
+    }
+  } catch (error) {
+    return handleError(res, error, "followUnfollowUser");
+  }
+};
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,7 +1,15 @@
 import { Router } from "express";
-import { getUserProfile } from "../controllers/user.controller.js";
+import {
+  followUnfollowUser,
+  getUserProfile,
+} from "../controllers/user.controller.js";
 import { protectRoute } from "../middleware/protectRoute.js";
 
 export const userRoutes = Router();
 
 userRoutes.get("/profile/:username", protectRoute, getUserProfile);
+userRoutes.post(
+  "/follow/:followerId",
+  protectRoute,
+  followUnfollowUser,
+);
